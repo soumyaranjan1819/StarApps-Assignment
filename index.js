b39a4c0264994897d1edac8f13a4b039cf2f3152
[Soumya Ranjan] <soumyaranjan1819@gmail.com>
2023-01-31 14:02:10 +0530

    bug fixed

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const productImg = document.querySelector('#productImg')
 const logo = document.querySelector('#displayLogo')
 const fileName = document.querySelector('#fileName')
 const deleteButton = document.querySelector('.deleteFile')
+const heroImage = document.querySelector('.heroImage')
+const loader = document.querySelector('#loader')
 
 
 uploadBtn.addEventListener('change',()=>{
@@ -17,7 +19,7 @@ uploadBtn.addEventListener('change',()=>{
     }) 
 })
 deleteButton.addEventListener('click',()=>{
-    fileName.innerHTML= 'UPLOAD LOGO';
+    fileName.innerHTML= 'UPLOAD NEW LOGO';
     deleteButton.style.display='none'
 })
 
@@ -26,18 +28,44 @@ colorPickers.addEventListener('click',(e)=>{
     if(e.target.classList.contains('yellow')){
         bgClr.style.backgroundColor='#fffadf'
         inputBtn.style.backgroundColor='#ffcd05'
-        productImg.setAttribute('src','./assets/Yello umbrella.png')
+        productImg.style.display='none'
+        loader.style.display='block'
+        loader.style.fill='#ffcd05'
+        logo.style.display='none'
+        setTimeout(()=>{
+            productImg.style.display='block'
+            productImg.setAttribute('src','./assets/Yello umbrella.png')
+            loader.style.display='none'
+            logo.style.display='block'
+        },800)
     }
     if(e.target.classList.contains('pink')){
         bgClr.style.backgroundColor='#fee2e2'
         inputBtn.style.backgroundColor='#ff2d50'
-        productImg.setAttribute('src','./assets/Pink umbrella.png')
-        
+        productImg.style.display='none'
+        loader.style.display='block'
+        loader.style.fill='#ff2d50'
+        logo.style.display='none'
+        setTimeout(()=>{
+            productImg.style.display='block'
+            productImg.setAttribute('src','./assets/Pink umbrella.png')
+            loader.style.display='none'
+            logo.style.display='block'
+        },800)
     }
     if(e.target.classList.contains('blue')){
         bgClr.style.backgroundColor='#e0f2fe'
         inputBtn.style.backgroundColor='#00c8ff'
-        productImg.setAttribute('src','./assets/Blue umbrella.png')
+        productImg.style.display='none'
+        loader.style.display='block'
+        loader.style.fill='#00c8ff'
+        logo.style.display='none'
+        setTimeout(()=>{
+            productImg.style.display='block'
+            productImg.setAttribute('src','./assets/Blue umbrella.png')
+            loader.style.display='none'
+            logo.style.display='block'
+        },800)
     }
 })
 
